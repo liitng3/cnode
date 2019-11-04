0f2584b27539c2f198d559a2368b1b44f891abbe
liitng3 <898260697@qq.com>
2019-11-04 21:57:56 +0800

    add

---
+++ build/precache-manifest.9a8fe637d680d47356d2572e8be8b118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98430769ae3432368bfab7eef77421f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "220eb4fe723c176ce661",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "2ed0d2521e33d5907dfe",
+    "url": "/static/css/main.7b8f4d59.chunk.css"
+  },
+  {
+    "revision": "220eb4fe723c176ce661",
+    "url": "/static/js/2.aab7eaf5.chunk.js"
+  },
+  {
+    "revision": "2ed0d2521e33d5907dfe",
+    "url": "/static/js/main.7c82fec6.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
